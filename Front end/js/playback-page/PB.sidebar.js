7019c38ba73b24234c021281eb85df536cbff48a
parham0121 <parysolh@gmail.com>
2025-04-04 17:30:29 +0330

    fixed all bugs of sidebar to playback page

--- Front end/js/playback-page/PB.sidebar.js
+++ Front end/js/playback-page/PB.sidebar.js
@@ -1,13 +1,21 @@
 const menuBtn = document.querySelector(".menu-btn")
+const btnClosd = document.querySelector(".menu-btn-side")
 const sidebar = document.querySelector(".sidebar-container")
 const coverblack = document.querySelector(".cover-black")
 
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("close")
     coverblack.classList.toggle("active")
+    sidebar.style.top = "0"
+    sidebar.style.background= "#0f0f0f"
 });
 
 coverblack.addEventListener('click', () => {
     coverblack.classList.remove("active")
     sidebar.classList.add("close")
 });
+
+btnClosd.addEventListener("click", () => {
+    sidebar.classList.add("close")
+    coverblack.classList.remove("active")
+})
